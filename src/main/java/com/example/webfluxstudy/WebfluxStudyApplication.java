95dafb661c0c257a039a3dbb2af079e4283b78da
caporatang <caporatang@naver.com>
2023-09-25 23:40:48 +0900

    init project

---
+++ src/main/java/com/example/webfluxstudy/WebfluxStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webfluxstudy/WebfluxStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
