86b016e7dcb4575bdcbe039df682f3a60dd2d7b6
caporatang <caporatang@naver.com>
2023-10-02 21:43:44 +0900

    Feat : method Reference

---
+++ src/main/java/com/example/webfluxstudy/completableFuture/CompletableFuture.java
@@ -0,0 +1,20 @@
+package com.example.webfluxstudy.completableFuture;
+
+/**
+ * packageName : com.example.webfluxstudy.completableFuture
+ * fileName : CompletableFuture
+ * author : taeil
+ * date : 2023/10/02
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 2023/10/02        taeil                   최초생성
+ */
+public class CompletableFuture {
+    // CompletableFuture는 Future와 CompletionStage 라는 부제를 가지고 있다!
+
+    // 2014년에 발표된 java 8에서 처음 도입
+    // 비동기 프로그래밍 지원
+    // Lambda, Method reference 등 Java 8의 새로운 기능 지원
+}
