2483184e5f9b98a37ef705599edbeb6c2aec6109
caporatang <caporatang@naver.com>
2023-10-04 23:53:14 +0900

    Feat : done 상태가 아닌 상태에서 thenAccept vs thenAcceptAsync

---
+++ src/main/java/com/example/webfluxstudy/completableFuture/completionStage/CompletionStageThenAcceptAsyncRunningExample.java
@@ -0,0 +1,37 @@
+package com.example.webfluxstudy.completableFuture.completionStage;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletionStage;
+
+/**
+ * packageName : com.example.webfluxstudy.completableFuture.completionStage
+ * fileName : CompletionStageThenAcceptAsyncRunningExample
+ * author : taeil
+ * date : 2023/10/04
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 2023/10/04        taeil                   최초생성
+ */
+@Slf4j
+public class CompletionStageThenAcceptAsyncRunningExample {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        // ForkJoinPool 스레드 에서 -> 별도의 스레드에서 동작한다.
+        //
+
+        log.info("start main");
+        CompletionStage<Integer> stage = Helper.runningStage();
+
+        stage.thenAcceptAsync(i -> {
+            log.info("{} in thenAcceptAsync", i);
+        }).thenAcceptAsync(i -> {
+            log.info("{} in thenAcceptAsync", i);
+        });
+        Thread.sleep(2000);
+    }
+}
