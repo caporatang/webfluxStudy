131353873efcc46d7adf66cdc6d660e425851e53
caporatang <caporatang@naver.com>
2023-10-30 23:57:40 +0900

    Feat : JavaNIO를 비동기로 처리할 수 있는 방법 : SelectableChannel accept, connect

---
+++ src/main/java/com/example/webfluxstudy/javaIoNioAio/javaNIO/buffer/channel/selectableChannel/SelectableChannel.java
@@ -0,0 +1,23 @@
+package com.example.webfluxstudy.javaIoNioAio.javaNIO.buffer.channel.selectableChannel;
+
+/**
+ * packageName : com.example.webfluxstudy.javaIoNioAio.javaNIO.buffer.channel.selectableChannel
+ * fileName : SelectableChannel
+ * author : taeil
+ * date : 2023/10/30
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 2023/10/30        taeil                   최초생성
+ */
+public class SelectableChannel {
+    // JavaNIO를 non-blocking 하게 쓰려면?
+
+    // SelectableChannel
+    // SocketChannel, ServerSocketChannel 모두 AbstractSelectableChannel을 상속
+    // AbstractSelectableChannel은 SelectableChannel을 상속
+
+    // configureBlocking 과 register 함수 제공
+    // configureBlocking : serverSocketChannel의 accept, socketChannel의 connect 등이 non-blocking으로 동작한다.
+}
