437d87d91c44fd33d4eb949c5018df406aa70c30
caporatang <caporatang@naver.com>
2023-11-15 22:46:48 +0900

    Docs : netty

---
+++ src/main/java/com/example/webfluxstudy/netty/Netty.java
@@ -0,0 +1,21 @@
+package com.example.webfluxstudy.netty;
+
+/**
+ * packageName : com.example.webfluxstudy.netty
+ * fileName : Netty
+ * author : taeil
+ * date : 11/15/23
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 11/15/23        taeil                   최초생성
+ */
+public class Netty {
+    // 비동기 이벤트 기반의 네트워크 어플리케이션 프레임워크
+    // HTTP 뿐만 아니라 다양한 프로토콜을 지원
+    // JavaIO, NIO, selector 기반으로 적은 리소스로 높은 성능이 보장된다.
+    // 불필요한 메모리 copy를 최소한으로 사용한다.
+    // 유연하며 확장 가능한 이벤트 모델 기반이다.
+    // 서버와 클라이언트 모두 지원한다.
+}
