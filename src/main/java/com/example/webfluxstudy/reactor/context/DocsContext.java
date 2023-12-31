b24814729c2026ba4f20eedd9e84c9d73e3c6331
caporatang <caporatang@naver.com>
2023-12-31 17:58:09 +0900

    Docs : context

---
+++ src/main/java/com/example/webfluxstudy/reactor/context/DocsContext.java
@@ -0,0 +1,20 @@
+package com.example.webfluxstudy.reactor.context;
+
+/**
+ * packageName : com.example.webfluxstudy.reactor.context
+ * fileName : DocsContext
+ * author : taeil
+ * date : 12/31/23
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 12/31/23        taeil                   최초생성
+ */
+public class DocsContext {
+    // Context는 파이프라인 내부 어디에서든 접근 가능한 key value 저장소이다.
+    // 특정 key의 value에 접근하고 key의 value를 수정할 수 있는 수단을 제공한다
+    // map과 유사한 형태를 가지고 있다.
+    // 읽기 전용인 ContextView, 쓰기까지 할 수 있는 Context로 구분된다.
+    
+}
