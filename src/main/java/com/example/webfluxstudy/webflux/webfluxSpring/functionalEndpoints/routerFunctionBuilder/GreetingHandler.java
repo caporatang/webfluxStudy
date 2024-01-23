39646bcced515dbf5b456730688d69a58c01d598
caporatang <caporatang@naver.com>
2024-01-23 23:15:52 +0900

    Feat : RouterFunction 실행, RouterFunction이 동작하는 과정과 handlerFunction을 이용한 mapping과 adapter

--- src/main/java/com/example/webfluxstudy/webflux/webfluxSpring/functionalEndpoints/routerFunctionBuilder/GreetingHandler.java
+++ src/main/java/com/example/webfluxstudy/webflux/webfluxSpring/functionalEndpoints/routerFunctionBuilder/GreetingHandler.java
@@ -19,6 +19,7 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class GreetingHandler {
     public static Mono<ServerResponse> greetQueryParam(ServerRequest serverRequest) {
+        // serverRequest의 queryParam을 이용해서 name 필드에 접근
         String name = serverRequest.queryParam("name")
                 .orElse("world");
 
@@ -27,6 +28,7 @@ public class GreetingHandler {
     }
 
     public static Mono<ServerResponse> greetPathVariable(ServerRequest serverRequest) {
+        // pathVariable 로부터 name에 해당하는 값을 받는다.
         String name = serverRequest.pathVariable("name");
 
         String content = "Hello " + name;
@@ -34,20 +36,24 @@ public class GreetingHandler {
     }
 
     public static Mono<ServerResponse> greetJsonBody(ServerRequest serverRequest) {
+        // serverRequest.bodyToMono로 body를 NameHolder 객체로 만들고 getName 으로 이름에 접근
         return serverRequest.bodyToMono(NameHolder.class)
                 .map(NameHolder::getName)
                 .map(name -> "Hello " + name)
+                // mono를 까서 일반 mono를 반환
                 .flatMap(content -> ServerResponse.ok().bodyValue(content))
                 .doOnError(throwable -> log.error("error", throwable));
     }
 
     public static Mono<ServerResponse> greetPlainTextBody(ServerRequest serverRequest) {
+        // server.bodyToMono로 body를 String으로 읽어서 처리
         return serverRequest.bodyToMono(String.class)
                 .map(name -> "Hello " + name)
                 .flatMap(content -> ServerResponse.ok().bodyValue(content));
     }
 
     public static Mono<ServerResponse> greetHeader(ServerRequest serverRequest) {
+        // serverRequest.headers를 이용하여 X-Custom-Name 헤더를 읽어서 처리
         String name = serverRequest.headers().header("X-Custom-Name")
                 .stream()
                 .findFirst()
--- src/main/java/com/example/webfluxstudy/webflux/webfluxSpring/functionalEndpoints/routerFunctionBuilder/RouterFunctionExample.java
+++ src/main/java/com/example/webfluxstudy/webflux/webfluxSpring/functionalEndpoints/routerFunctionBuilder/RouterFunctionExample.java
@@ -2,8 +2,11 @@ package com.example.webfluxstudy.webflux.webfluxSpring.functionalEndpoints.route
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.netty.http.server.HttpServer;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -29,8 +32,8 @@ public class RouterFunctionExample {
     // 네 번째 POST : contentType 을 확인
 
     // path와 nest는 아래 일종의 파이프라인들의 중첩을 만드는 역할을하는것임.
-    
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
         RouterFunction<ServerResponse> router = route()
                 .path("/greet", b1 -> b1
                         // header가 Text-plain을 accept할 수 있으면 아래쪽으로 요청을 넘기겠다.
@@ -47,5 +50,29 @@ public class RouterFunctionExample {
                         )
                 )
                 .build();
+
+        // RouterFunction 실행
+        // RotuerFunctions.toHttpHadnelr로 httpHandler로 변경
+        // ReactorHandlerAdapter로 HttpServer에 전달
+        var httpHandler = RouterFunctions.toHttpHandler(router); // RouterFunctions : 스프링을 쓰지 않더라도 httpHandler를 만들수있게 해주는 클래스
+        var adapter = new ReactorHttpHandlerAdapter(httpHandler);
+
+        HttpServer.create()
+                .host("localhost")
+                .port(8080)
+                .handle(adapter)
+                .bindNow()
+                .channel().closeFuture().sync();
     }
+
+    // RouterFunction in Spring
+    // 총 RouterFunction 이 동작하는 과정
+    // 1. ServerWebExchange로 요청이 들어오면, RouterFunction이 bean으로 등록되고 RouterFunctionMapping을 통해서 handlerFunction 반환
+    // 2. HandlerFunctionAdapter를 통해서 handlerFunction을 실행하고 ServerResponse를 HandlerResult(ServerResponse)에 담아서 반환한다.
+    // 3. ServerResponseResultHandler가 HandlerResult로 render -> writeTo
+
+    // handlerFunction을 이용한 mapping과 adapter
+    // RouterFunctionMapping은 HandlerFunction을 반환한다.
+    // HandlerFunction은 HandlerFunctionAdapter에서 내부적으로 실행되고
+    // HandlerFunction에서 만든 결과인 ServerResponse는 ServerResponseResultHandler로 전달되어 요청이 나가게 된다.
 }
