075bf204e38c0991293a6dc08518916103d12d71
caporatang <caporatang@naver.com>
2024-02-13 22:39:10 +0900

    Server Sent Event (외부에서 사용하기)

--- src/main/java/com/example/webfluxstudy/serverSentEvent/controller/NotificationController.java
+++ src/main/java/com/example/webfluxstudy/serverSentEvent/controller/NotificationController.java
@@ -1,6 +1,8 @@
 package com.example.webfluxstudy.serverSentEvent.controller;
 
 import com.example.webfluxstudy.serverSentEvent.domain.Event;
+import com.example.webfluxstudy.serverSentEvent.service.NotificationService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
@@ -25,19 +27,21 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Slf4j
 @RequestMapping("/api/notifications")
 @RestController
+@RequiredArgsConstructor
 public class NotificationController {
 
     // Sink ?
     // sink에 특정 값들을 저장할 수 있는데 Flux로 변환해서 흘려줄수있게 해준다.
     // sink 의 many는 Flux -> 싱글 subscribe 처리
-    private static Sinks.Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
+    // private static Sinks.Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
 
+    private final NotificationService notificationService;
     private static AtomicInteger lastEventId = new AtomicInteger(1);
 
     // ServerSendEvent 에서는 반드시 produces를 event value로 명시 해주어야한다.
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ServerSentEvent> getNotifications() {
-        return sink.asFlux()
+        return notificationService.getmeesageFromSink()
                 .map(message -> {
                     String id = lastEventId.getAndIncrement() + "";
                     return ServerSentEvent
@@ -53,12 +57,10 @@ public class NotificationController {
 
     @PostMapping
     public Mono<String> addNotification(@RequestBody Event event) {
-        String newNotificationMessage = event.getType() + " : " + event.getMessage();
-        log.info("message : {}", newNotificationMessage);
-        sink.tryEmitNext(newNotificationMessage);
-
+        // 결국! 외부에서 sink를 사용할때도 마찬가지다. 그냥 호출해서 사용하면 된다
+        String message = event.getType() + " : " + event.getMessage();
+        notificationService.tryEmitNext(message);
         return Mono.just("ok");
-
     }
 
 }
--- src/main/java/com/example/webfluxstudy/serverSentEvent/service/NotificationService.java
+++ src/main/java/com/example/webfluxstudy/serverSentEvent/service/NotificationService.java
@@ -1,5 +1,6 @@
 package com.example.webfluxstudy.serverSentEvent.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
@@ -15,13 +16,21 @@ import reactor.core.publisher.Sinks;
  * -------------------------------------------------------
  * 2/13/24        taeil                   최초생성
  */
+@Slf4j
 @Service
 public class NotificationService {
     // 다른 bean 에서, 컨트롤러 밖에서 Sink를 사용하려면?
+    // 결국! 외부에서 sink를 사용할때도 마찬가지다. 그냥 호출해서 사용하면 된다
     private static Sinks.Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
 
     public Flux<String> getmeesageFromSink() {
-        return null;
+        return sink.asFlux();
+    }
+
+    public void tryEmitNext(String message) {
+        log.info("message : {}", message);
+        sink.tryEmitNext(message);
+
     }
 
 }
