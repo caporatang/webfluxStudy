3dd700deccb29a9cb2942bd050dd146f37593fbd
caporatang <caporatang@naver.com>
2024-02-17 22:49:06 +0900

    chore : chatApplicationExample 모듈 생성

---
+++ chatApplicationExample/src/main/java/com/example/chatapplicationexample/ChatApplicationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatapplicationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplicationExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplicationExampleApplication.class, args);
+    }
+
+}
---
+++ chatApplicationExample/src/test/java/com/example/chatapplicationexample/ChatApplicationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatapplicationexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
