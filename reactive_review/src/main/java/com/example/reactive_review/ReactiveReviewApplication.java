395fe12a3c34206eceb1519363a96bb5daf85791
caporatang <caporatang@naver.com>
2024-04-15 22:33:13 +0900

    chore : reactive_review module init

---
+++ reactive_review/src/main/java/com/example/reactive_review/ReactiveReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactive_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveReviewApplication.class, args);
+    }
+
+}
---
+++ reactive_review/src/test/java/com/example/reactive_review/ReactiveReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactive_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
